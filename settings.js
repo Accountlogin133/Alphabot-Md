b30abdd25a8b5ef8a3f1dc37e1b219879b93727f
Accountlogin133 <106410323+Accountlogin133@users.noreply.github.com>
2023-01-08 10:24:29 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,13 +13,13 @@ global.APIs = {
 
 // Free apikey (Apikey expired silahkan login terus ganti V12 dgn apikey lu)
 global.APIKeys = {
-   'https://api.zeeoneofc.my.id': 'V12', // 👉 login https://api.zeeoneofc.xyz to get apikey
+   'https://api.zeeoneofc.my.id': 'ydofVS76', // 👉 login https://api.zeeoneofc.xyz to get apikey
 }
 
 // setting 
 global.autoread = false // auto read message
 global.autobio = false //auto ganti bio
-global.anticall = true //anti call 
+global.anticall = false //anti call 
 global.userRegister = false
 global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
 
@@ -31,24 +31,24 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@ChZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "©DRAGONBOTZ™" //namabot kalian
+global.ownername = "⚡𝙋𝙐𝙏𝙍𝘼 𝙂𝘼𝙈𝙄𝙉𝙂⚡" //nama kalian
+global.myweb = "gada" //bebas asal jan hapus
+global.youtube = "gada" //bebas asal jan hapus
+global.myweb2 = "gada" //bebas
+global.email = "putrahaqimi344@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
 global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.owner = ["601121075279", "601121075279"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "601121075279" // nomor wa kalian
+global.ownernomerr = "+601121075279" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '©DRAGONBOTZ™' //sticker wm ubah
+global.author = 'Di Buat Oleh ⚡𝙋𝙐𝙏𝙍𝘼 𝙂𝘼𝙈𝙄𝙉𝙂⚡' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
